e210568336f0430b418a2fb95624b7e693b2c102
brixDevWork <brixDevWork@Graces-Mac-mini.local>
2022-06-06 18:38:40 +0800

    initialcommit

---
+++ SocialMedia2-brixbranch/src/pages/Profile/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PostSide from '../../components/PostSide/PostSide'
+import ProfileCard from '../../components/ProfileCard.jsx/ProfileCard'
+import ProfileLeft from '../../components/ProfileLeft/ProfileLeft'
+import './Profile.css'
+import RightSide from '../../components/RightSide/RightSide'
+
+const Profile = () => {
+  return (
+    <div className="Profile">
+        <ProfileLeft/>
+
+        <div className="Profile-center">
+          <ProfileCard/>
+          <PostSide/>
+          
+        </div>
+
+        <RightSide/>
+        </div>
+  )
+}
+
+export default Profile
